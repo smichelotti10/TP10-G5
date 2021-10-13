8cb82f4dd0efcbc6549b1fddf50ccd826d08bb56
smichelotti <smichelotti@itba.edu.ar>
2021-10-13 11:05:01 -0300

    Prueba desde Netbeans

---
+++ TP10/main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: santiago
+ *
+ * Created on October 13, 2021, 10:58 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("Hola Mundo\n");
+    return (EXIT_SUCCESS);
+}
+
